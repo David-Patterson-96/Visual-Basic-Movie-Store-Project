c735d906cc558456460f513ebc481a3d5b589a55
David A Patterson <78324996+David-Patterson-96@users.noreply.github.com>
2021-08-03 09:58:49 -0500

    Add files via upload

---
+++ Final Project/Film Soulution/Film Project/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Film Project")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Copper Mountain College")>
+<Assembly: AssemblyProduct("Film Project")>
+<Assembly: AssemblyCopyright("Copyright © Copper Mountain College 2016")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("534ae1e9-0766-41d4-9b1c-0284cdc426b1")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
